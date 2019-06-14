ca43ba90a5ddc4de9b2ad11a5937128cd9faea7a
Kevin Logan <kevlogan7@gmail.com>
2019-06-14 01:13:28 -0400

    Separate out navbar into it's own component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/transactions">Transactions</router-link> |
-      <router-link to="/modal">Modal</router-link> |
-      <router-link to="/loadingIndicator">Loading Indicator</router-link> | 
-      <router-link to="/image">Image</router-link> |
-      <router-link to="/semantics">Semantics</router-link>
-    </div>
-    <router-view />
+  <div>
+    <NavBar />
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: "app",
+  components: {
+    NavBar
+  }
+};
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar has-shadow is-spaced ">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="../assets/logo.png" />
+        </a>
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <router-link role="button" class="navbar-item" to="/">Home</router-link>
+          <router-link role="button" class="navbar-item" to="/transactions">Transactions</router-link>
+          <router-link role="button" class="navbar-item" to="/modal">Modal</router-link>
+          <router-link role="button" class="navbar-item" to="/loadingIndicator">Loading Indicator</router-link>
+          <router-link role="button" class="navbar-item" to="/image">Image</router-link>
+          <router-link role="button" class="navbar-item" to="/semantics">Semantics</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>

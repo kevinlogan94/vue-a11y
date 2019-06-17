fc598c5a093289772a44850c5779ed72b88f90d4
Kevin Logan <kevlogan7@gmail.com>
2019-06-17 09:12:55 -0400

    Prettier/lint changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,7 +5,13 @@
         <a class="navbar-item" href="https://bulma.io">
           <img src="../assets/logo.png" />
         </a>
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -13,12 +19,24 @@
       </div>
       <div id="navbarBasicExample" class="navbar-menu">
         <div class="navbar-start">
-          <router-link role="button" class="navbar-item" to="/">Home</router-link>
-          <router-link role="button" class="navbar-item" to="/transactions">Transactions</router-link>
-          <router-link role="button" class="navbar-item" to="/modal">Modal</router-link>
-          <router-link role="button" class="navbar-item" to="/loadingIndicator">Loading Indicator</router-link>
-          <router-link role="button" class="navbar-item" to="/image">Image</router-link>
-          <router-link role="button" class="navbar-item" to="/semantics">Semantics</router-link>
+          <router-link role="button" class="navbar-item" to="/"
+            >Home</router-link
+          >
+          <router-link role="button" class="navbar-item" to="/transactions"
+            >Transactions</router-link
+          >
+          <router-link role="button" class="navbar-item" to="/modal"
+            >Modal</router-link
+          >
+          <router-link role="button" class="navbar-item" to="/loadingIndicator"
+            >Loading Indicator</router-link
+          >
+          <router-link role="button" class="navbar-item" to="/image"
+            >Image</router-link
+          >
+          <router-link role="button" class="navbar-item" to="/semantics"
+            >Semantics</router-link
+          >
         </div>
       </div>
     </div>

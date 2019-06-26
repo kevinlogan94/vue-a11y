0b24a3087dec99bb4556f58692bf8c91c5fc15f2
Kevin Logan <kevlogan7@gmail.com>
2019-06-26 17:38:44 -0400

    :bug: fix unknown custom element error

--- src/main.js
+++ src/main.js
@@ -2,20 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import VueA11yDialog from "vue-a11y-dialog";
+import VueSkipTo from "vue-skip-to";
+
 import "bulma/css/bulma.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueA11yDialog);
+Vue.use(VueSkipTo);
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
-
-import VueA11yDialog from "vue-a11y-dialog";
-
-Vue.use(VueA11yDialog);
-
-// import Vue from 'vue'
-import VueSkipTo from "vue-skip-to";
-
-Vue.use(VueSkipTo);
